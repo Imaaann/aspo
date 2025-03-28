8063567921f9d4c6538b9c941e1e5c2b385ad7de
Imaaann <iman555bpe147@gmail.com>
2025-03-28 15:38:34 +0100

    feat(cli): made the app accept the flags and path

--- cli/src/main/java/com/aspodev/cli/AspoCommand.java
+++ cli/src/main/java/com/aspodev/cli/AspoCommand.java
@@ -1,23 +1,31 @@
 package com.aspodev.cli;
 import java.lang.Runnable;
-import picocli.CommandLine.Option;
 
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command (
+    name = "Aspo",
+    mixinStandardHelpOptions = true,
+    version = "Aspo@0.0.1",
+    description = "A static code analyzer for java projects"
+)
 public class AspoCommand implements Runnable {
 
-    @Option(names = { "-v", "--verbose" }, description = "Enable Verbose mode.") 
-    private boolean verbose;
-
-    @Option(names =  {"-n", "--none"}, description = "Removes the file ouputs")
+    @Option (names = {"-nf", "--no-files"}, description = "Makes the program not output any files")
     private boolean noFileOutputs;
+
+    @Option (names = {"-m", "--more"}, description = "Adds more statistics to the analysis")
+    private boolean moreMetrics;
+
+    @Parameters (index = "0", description= "The PATH/URL of the repository to scan")
+    private String targetPath;
 
     @Override
     public void run() {
-        if (verbose) {
-            System.out.println("Verbose mode activated");
-        }
-
-        if (noFileOutputs) {
-            System.out.println("no file outputs will be shown");
-        }
+        System.out.println("Starting analysis of: " + targetPath);
+        System.out.println("More flag enabled? " + moreMetrics);
+        System.out.println("Hidden output files? " + noFileOutputs);
     }
 }
