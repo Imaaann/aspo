3d283ee618d812c8501987d01177dc3a45f15106
Chikh Mohamed Amine <chikhmedamine433@gmail.com>
2025-04-03 13:19:42 +0100

    feat(PathResolver): add getAllJavaPaths method that returns a list of all the java file paths

--- cli/src/main/java/com/aspodev/cli/PathResolver.java
+++ cli/src/main/java/com/aspodev/cli/PathResolver.java
@@ -1,10 +1,14 @@
 package com.aspodev.cli;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class PathResolver {
-
     private static boolean isValidPath(Path path) { // this method checks if a path is valid
         try {
             path.toRealPath(); // try to convert the path to a real path
@@ -33,4 +37,15 @@ public class PathResolver {
         return resolvePath(path).toString();
     }
 
+    public static List<String> getJAllJavaPaths(String path) { // this method is used to get all .java files paths in a directory and its subdirectories
+        List<String> javaFilePaths = new ArrayList<>(); // this list will hold the paths of all .java files
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) { // this stream will walk through the directory and its subdirectories
+            paths.filter(Files::isRegularFile) // filter the paths to only include regular files
+                     .filter(p -> p.toString().endsWith(".java")) // filter the paths to only include .java files
+                     .forEach(p -> javaFilePaths.add(p.normalize().toString())); // add the paths to the list
+        } catch (IOException e) { // if an exception is thrown, print the stack trace
+            e.printStackTrace();
+        }
+            return javaFilePaths; // return the list of .java file paths
+    }
 }
