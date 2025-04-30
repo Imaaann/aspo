414ae1e31ac41bd910848ff9d2ee53b71b76b0f3
Imad Ishak Chouia <155010962+Imadch7@users.noreply.github.com>
2025-04-30 14:55:19 +0100

    Delete web/src/main/java/com/aspodev/web directory

--- web/src/main/java/com/aspodev/web/Exception/UrlException.java
+++
@@ -1,9 +0,0 @@
-package com.aspodev.web.Exception;
-
-public class UrlException extends RuntimeException{
-    private String message;
-    UrlException(String message){
-        super();
-        this.message = message;
-    }
-}
