e71167813ecf5f523e1241b4499f6f07016250c6
Imaaann <iman555bpe147@gmail.com>
2025-05-04 14:46:00 +0100

    feat(inst): added getModifiers() method

--- cli/src/main/java/com/aspodev/parser/Behavior/BehaviorManager.java
+++ cli/src/main/java/com/aspodev/parser/Behavior/BehaviorManager.java
@@ -44,7 +44,6 @@ public class BehaviorManager {
 
 	public void execute(ParserContext context, Instruction instruction) {
 		registry.getOrDefault(instruction.getType(), (c, i) -> {
-
 			System.out.println("[DEBUG] == unhandled instruction: " + instruction);
 		}).apply(context, instruction);
 	}
--- cli/src/main/java/com/aspodev/parser/Instructions/Instruction.java
+++ cli/src/main/java/com/aspodev/parser/Instructions/Instruction.java
@@ -1,7 +1,9 @@
 package com.aspodev.parser.Instructions;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.aspodev.SCAR.Modifier;
 import com.aspodev.parser.Token;
 import com.aspodev.parser.TokenNotFoundException;
 
@@ -25,6 +27,30 @@ public class Instruction {
 		return Instruction.getIdentifier(tokens, index);
 	}
 
+	public List<Modifier> getModifiers() {
+		List<Modifier> result = new ArrayList<>();
+		boolean normalClassDeclaration = isTypeDeclaration();
+
+		for (Token token : tokens) {
+			if (isModifier(token))
+				result.add(Modifier.convert(token.getValue()));
+
+			if (normalClassDeclaration) {
+				if (token.getValue().equals("sealed"))
+					result.add(Modifier.SEALED);
+
+				if (token.getValue().equals("non")) {
+					int nonPosition = token.getPosition();
+					if (tokens.get(nonPosition + 1).getValue().equals("-")
+							&& tokens.get(nonPosition + 2).getValue().equals("sealed"))
+						result.add(Modifier.NON_SEALED);
+				}
+			}
+		}
+
+		return result;
+	}
+
 	public List<Token> getTokens() {
 		return this.tokens;
 	}
@@ -43,5 +69,18 @@ public class Instruction {
 
 	public String toString() {
 		return tokens + " TYPE:" + type;
+	}
+
+	private boolean isTypeDeclaration() {
+		return type == InstructionTypes.CLASS_DECLARATION || type == InstructionTypes.RECORD_DEFINITION
+				|| type == InstructionTypes.ENUM_DECLARTION || type == InstructionTypes.INTERFACE_DECLARATION;
+	}
+
+	private boolean isModifier(Token token) {
+		String value = token.getValue();
+
+		return value.equals("abstract") || value.equals("synchronized") || value.equals("transient")
+				|| value.equals("final") || value.equals("static") || value.equals("strictfp")
+				|| value.equals("volatile");
 	}
 }
