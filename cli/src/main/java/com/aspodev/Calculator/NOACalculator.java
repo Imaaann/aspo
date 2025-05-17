8a06c4c3fa35a95667371809e2e0456ffff4fcc4
Mouadke <mouadkennouche@gmail.com>
2025-05-17 14:52:21 +0100

    added NOA Calculator + added getMapSlices method in the model

---
+++ cli/src/main/java/com/aspodev/Calculator/NOACalculator.java
@@ -0,0 +1,16 @@
+package com.aspodev.Calculator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.aspodev.SCAR.Model;
+
+public class NOACalculator implements MetricCalculator {
+    @Override
+    public Map<String, Double> calculate(Model SCAR) {
+        Map<String, Double> result = new HashMap<>();
+        for(SCAR.)
+        return null;
+        
+    }
+}
