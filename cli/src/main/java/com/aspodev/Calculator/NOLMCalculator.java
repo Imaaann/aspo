09a4474821b1ea10414fd9fbbc544317651d8fe5
Mouadke <mouadkennouche@gmail.com>
2025-05-19 15:35:09 +0100

    splited the function

--- cli/src/main/java/com/aspodev/Calculator/NOLMCalculator.java
+++ cli/src/main/java/com/aspodev/Calculator/NOLMCalculator.java
@@ -18,27 +18,33 @@ public class NOLMCalculator implements MetricCalculator {
         Map<String, Slice> slicesMap = SCAR.getSliceMap();
         int numOfOverLoadedMethods = 0;
         for (Map.Entry<String, Slice> i : slicesMap.entrySet()) {
-            numOfOverLoadedMethods = 0;
-            Set<String> checked = new HashSet<>();
-            Iterator<Method> methods = i.getValue().getMethods().iterator();
-            while (methods.hasNext()) {
-                Method method = methods.next();
-                if (!checked.contains(method.getName())) {
-                    checked.add(method.getName());
-                    Iterator<Method> OLcheck = i.getValue().getMethods().iterator();
-                    while (OLcheck.hasNext()) {
-                        Method OLmethod = OLcheck.next();
-                        if (method.getName().equals(OLmethod.getName()) && method != OLmethod) {
-                            numOfOverLoadedMethods++;
+            numOfOverLoadedMethods = getOverloadedMethodsCount(i.getValue());
 
-                        }
-                    }
-                }
-            }
             System.out.println("[DEBUG] NOLM result: " + numOfOverLoadedMethods + " For class: " + i.getKey());
             result.put(i.getKey(), Double.valueOf(numOfOverLoadedMethods));
         }
         return result;
+    }
+
+    private int getOverloadedMethodsCount(Slice slice) {
+        int numOfOverLoadedMethods = 0;
+        Set<String> checked = new HashSet<>();
+        Iterator<Method> methods = slice.getMethods().iterator();
+        while (methods.hasNext()) {
+            Method method = methods.next();
+            if (!checked.contains(method.getName())) {
+                checked.add(method.getName());
+                Iterator<Method> OLcheck = slice.getMethods().iterator();
+                while (OLcheck.hasNext()) {
+                    Method OLmethod = OLcheck.next();
+                    if (method.getName().equals(OLmethod.getName()) && method != OLmethod) {
+                        numOfOverLoadedMethods++;
+
+                    }
+                }
+            }
+        }
+        return numOfOverLoadedMethods;
     }
 
 }
