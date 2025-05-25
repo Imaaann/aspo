6bd049cd6994700a431c79310b03a6301aa6bd44
Imaaann <iman555bpe147@gmail.com>
2025-05-25 14:26:37 +0100

    fix(DTO): made the dto output CAPITAL metrics

--- cli/src/main/java/com/aspodev/DTO/SystemMetricsDTO.java
+++ cli/src/main/java/com/aspodev/DTO/SystemMetricsDTO.java
@@ -3,15 +3,21 @@ package com.aspodev.DTO;
 import com.aspodev.Calculator.SystemMetrics;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SystemMetricsDTO {
     private double NOC;
+
     private double NOI;
+
     private double MHF;
+
     private double AHF;
+
     private double TLOC;
+
     private QualityDTO quality;
 
     public SystemMetricsDTO(SystemMetrics systemMetrics, QualityDTO quality) {
@@ -24,22 +30,27 @@ public class SystemMetricsDTO {
         this.quality = quality;
     }
 
+    @JsonProperty("NOC")
     public double getNOC() {
         return NOC;
     }
 
+    @JsonProperty("NOI")
     public double getNOI() {
         return NOI;
     }
 
+    @JsonProperty("MHF")
     public double getMHF() {
         return MHF;
     }
 
+    @JsonProperty("AHF")
     public double getAHF() {
         return AHF;
     }
 
+    @JsonProperty("TLOC")
     public double getTLOC() {
         return TLOC;
     }
